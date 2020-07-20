723d72b5dd6336bc340271216f8eb3f6c1bb88a4
william-parker <edwinious@gmail.com>
2020-07-19 23:25:11 -0400

    ex1.py & ex2.py completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,22 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # create cache
+    cache = {}
+
+    # make the keys the weights and the values the indices
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    # print(cache)
+    # for each weight find the difference
+    # if it matches, return the indices 
+    for i in range(len(weights)):
+        diff = limit-weights[i]
+        if diff in cache:
+            return (max(i, cache[diff]), min(i, cache[diff]))
 
     return None
+
+# ouput should be [3,1]
+weights = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights, 5, 21))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,3 +1,32 @@
+# Pseudocode for an array of `Tickets` might look like this:
+
+# ```
+# tickets = [
+#     Ticket{ source: "PIT", destination: "ORD" },
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
+# ```
+
+# Your function should output an array of strings with the entire route of
+# your trip. For the above example, it should look like this:
+
+# ```
+# ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+# ```
+
+# Your solution should run in linear time. You can assume that your
+# function will always be handed a valid ticket chain as input. 
+
+
+
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
@@ -10,5 +39,47 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    # hash table stores the route's airport source & destination info
+    cache = {}
+    # init route array
+    # next we need to add the original source airport
+    route = [None] * length
+
+    # iterate thru each ticket
+    # store its destination in the ht
+    for t in tickets:
+        cache[t.source] = t.destination
+
+    # starting point: first ticket along the route has NONE source
+    dest = cache["NONE"]
+
+    # next we find each ticket's next destination and add it to the destinations array
+    # flight is current ticket
+    for flight in range(length):
+        # record value of next destination
+        route[flight] = dest
+        # the next destination is the value in the ht, the key is the destination just added to the array 
+        # aka the next flight in the trip
+        # continue until complete
+        dest = cache[dest]
 
     return route
+
+
+# testing
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5, ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+length = len(tickets)
+
+print(reconstruct_trip(tickets, length)) 
